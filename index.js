43dba3ef86db9039f72d0ecbd95c7d4986559a10
DebFullstack <debbieking1979@gmail.com>
2023-05-18 20:22:55 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+class Car {
+	constructor (make, model, year) {
+		this.make = make;
+		this.model = model;
+		this.year = year;
+	}
+	getDescription () {
+		return `Make: ${this.make}; Model: ${this.model}; Year: ${this.year}`;
+	}
+}
 
+class ElectricCar extends Car {
+	constructor(make, model, year, range) {
+		super(make, model, year);
+		this.range = range;
+	}
+	getDescription () {
+		return `Make: ${this.make}; Model: ${this.model}; Year: ${this.year}; Range: ${this.range}`;
+	}
+}
+
+const Tesla = new ElectricCar (`Tesla`, `Model S`, `2019`, '300');
+
+console.log(Tesla.getDescription())
